9f463cb4d3a1bead5eb7dfc820b677fcffe034a1
Ananthakrishnan21 <ananthakrishnan430@gmail.com>
2023-03-16 22:55:49 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function myPromise(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Hello, world!")
+        },1000)
+    })
+}
+myPromise()
+    .then(update)
+    .catch((e)=>{console.log(e)})
+
+
+function update(data){
+    let ele=document.getElementById("output")
+    ele.innerText=data
+}
